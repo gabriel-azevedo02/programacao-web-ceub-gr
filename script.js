b5626ecc5d56009ab47346c9eea9011c39205238
gabriel-azevedo02 <gabrielribeiroazevedo02@gmail.com>
2024-03-21 10:26:12 -0300

    for loop

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
-let var1 = "30";
-let var2 = 5;
-
-let resultado1 = var1 - var2;
-let resultado2 = var1 + var2;
+let meuArray = ["cachorro", "gato", "coelho", "calopsita", "cobra", "cavalo", "dinossauro"];
 
 
-console.log(resultado1);
-console.log(typeof(resultado1));
+console.log(meuArray.length);
 
-console.log(resultado2);
-console.log(typeof(resultado2));
+for (let i = 0; i < meuArray.length; i++) {
+console.log(meuArray[i]);
+
+}
+
+
+
+
+
+
+
+// let var1 = 0.1;
+// let var2 = 0.2;
+
+// let resultado1 = var1 - var2;
+// let resultado2 = var1 + var2;
+
+
+// console.log(resultado1);
+// console.log(typeof(resultado1));
+
+// console.log(resultado2);
+// console.log(typeof(resultado2));
+
+
+
+
+// let variavelArray = [5, 4, 8, 0, -7, "abc", 8.3];
+
+// console.log(variavelArray[5]);
+
+
+// let variavelObjeto = {
+//     nome: "Gabriel",
+//     idade: "18",
+//     altura: 1.75,
+//     cpf: "6546846465",
+
+//     endereco: {
+//         rua: "ff",
+//         numero: 1,
+//         bairro: "vila",
+//     }
+
+// }
+
+// console.log(variavelObjeto["cpf"]);
+// console.log(variavelObjeto);
+
+
+// if (variavelObjeto.idade === 18) {
+//     console.log("Pode entrar no meu site");
+
+// } else {
+
+//     console.log("Não pode entrar no meu site");
+// }
 
 // console.log(var1 - var2);
 // console.log(var1 + var2);
