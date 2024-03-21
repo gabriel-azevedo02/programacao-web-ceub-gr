02aea9aa5ff88aa5c037e3653fda1e02a88e1db9
gabriel-azevedo02 <gabrielribeiroazevedo02@gmail.com>
2024-03-21 09:02:12 -0300

    Aula javaScript

---
+++ script.js
@@ -0,0 +1,34 @@
+let var1 = "30";
+let var2 = 5;
+
+let resultado1 = var1 - var2;
+let resultado2 = var1 + var2;
+
+
+console.log(resultado1);
+console.log(typeof(resultado1));
+
+console.log(resultado2);
+console.log(typeof(resultado2));
+
+// console.log(var1 - var2);
+// console.log(var1 + var2);
+
+
+
+
+
+// let variavel1 = 0;
+
+// variavel1 = "qualquercoisa";
+
+// const PI = 3.14;
+
+// console.log(typeof(variavel1));
+
+
+
+
+
+// console.log("gabrielbrabooo");
+// console.warn("gabrielbrabissimo")
